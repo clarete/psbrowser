922bcb00e14a4dd7aac899fe022c2ba9a47105a5
Lincoln de Sousa <lincoln@comum.org>
2010-05-07 11:10:15 -0300

    Fixes to the node listing feature
    Now the recursive node listing works as expected (I think, at least). The
    `update_node_list()' method was removed and `parse_list_nodes()' does
    the trick to call `list_nodes()' to childs of already found nodes.

--- gui/gui.py
+++ gui/gui.py
@@ -118,25 +118,19 @@ class MainWindow(gtk.Builder):
         self.logger.info('Auth failed callback running')
         self.unref_loading()
 
-    def update_node_list(self, nodes, parent_name, parent_iter):
-        model = self.treeview.get_model()
-        model.clear()
-        for node in nodes:
-            appended = model.append(parent_iter, [node])
-            print node, parent_name, parent_iter
-            self.list_nodes(node, appended)
-            break
-
     # pubsub stuff
 
     def parse_list_nodes(self, stanza, user_data):
-        nodes = []
+        model = self.treeview.get_model()
         node = stanza.find('query').child()
-        while node is not None:
-            nodes.append(node.find_attrib('node'))
+        parent_name, parent_iter = user_data
+        while node:
+            node_name = node.find_attrib('node')
+            if not node_name:
+                break
+            node_iter = model.append(parent_iter, [node_name])
+            self.list_nodes(node_name, node_iter)
             node = node.next()
-        node_name, node_iter = user_data
-        self.update_node_list(nodes, node_name, node_iter)
 
     def list_nodes(self, node_name=None, node_iter=None):
         args = (self.jid_from, self.jid_to)
